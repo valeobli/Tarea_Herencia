30aec2bfb8d617819ff78965a482588acf17ffab
valeobli <a01285498@tec.mx>
2025-01-16 22:09:04 +0000

    final

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 int main() {
-    Paquete paquete("Lulu", "Lila", "calle A", "calle B", 10, 20, 30, 40, 50, 60);
-    cout << "Costo del paquete: " << paquete.calculaCosto() << endl;
+    Paquete paquete("Lulu", "Lila", "calle 1", "calle 2", 10, 20, 30, 40, 50, 60);
+    cout << "costo paquete: " << paquete.calculaCosto() << endl;
 
-    Sobre sobre("Lalo", "Lola", "Calle C", "Calle D", 10, 20, 30, 10);
-    cout << "Costo del sobre: " << sobre.calculaCosto() << endl;
+    Sobre sobre("Lalo", "Lola", "Calle 3", "Calle 4", 10, 20, 30, 10);
+    cout << "costo sobre: " << sobre.calculaCosto() << endl;
 
     return 0;
 }
